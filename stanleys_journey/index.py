9fbaca7295f818c14146743135f4dbc8cb41a320
Otto.Stegmaier <Ostegmaier@fitbit.com>
2017-01-30 22:50:04 -0800

    Added some story forking and moved to the flask-ask template

--- stanleys_journey/index.py
+++ stanleys_journey/index.py
@@ -1,172 +1,106 @@
-from __future__ import print_function
-
-def lambda_handler(event, context):
-    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
-    etc.) The JSON body of the request is provided in the event parameter.
-    """
-    print("event.session.application.applicationId=" +
-          event['session']['application']['applicationId'])
-
-    """
-    Uncomment this if statement and populate with your skill's application ID to
-    prevent someone else from configuring a skill that sends requests to this
-    function.
-    """
-    # if (event['session']['application']['applicationId'] !=
-    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
-    #     raise ValueError("Invalid Application ID")
-
-    if event['session']['new']:
-        on_session_started({'requestId': event['request']['requestId']},
-                           event['session'])
-
-    if event['request']['type'] == "LaunchRequest":
-        return on_launch(event['request'], event['session'])
-    elif event['request']['type'] == "IntentRequest":
-        return on_intent(event['request'], event['session'])
-    elif event['request']['type'] == "SessionEndedRequest":
-        return on_session_ended(event['request'], event['session'])
+import logging
+from flask import Flask, render_template
+from flask_ask import Ask, request, session, question, statement, version
 
 
-def on_session_started(session_started_request, session):
-    """ Called when the session starts """
-
-    print("on_session_started requestId=" + session_started_request['requestId']
-          + ", sessionId=" + session['sessionId'])
+app = Flask(__name__)
+ask = Ask(app, "/")
+logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
 
-def on_launch(launch_request, session):
-    """ Called when the user launches the skill without specifying what they
-    want
-    """
-
-    # Dispatch to your skill's launch
-    return get_welcome_response()
+@ask.launch
+def launch():
+    speech_output = render_template('Welcome')
+    card_title = "Welcome to Stanley's Journey"
+    return question(speech_output).simple_card(title=card_title, content=speech_output).reprompt(speech_output)
 
 
-def on_intent(intent_request, session):
-    """ Called when the user specifies an intent for this skill """
-
-    print("on_intent requestId=" + intent_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
-
-    intent = intent_request['intent']
-    intent_name = intent_request['intent']['name']
-
-    # Dispatch to your skill's intent handlers
-    if intent_name == "TheBeginning":
-        return stanleys_history()
-    elif intent_name == "WhatHappenedNext":
-        return what_happened_next()
-    elif intent_name == "HowDoesItEnd":
-        return how_does_it_end()
-    elif intent_name == "AMAZON.HelpIntent":
-        return get_help_response()
-    elif intent_name in ["AMAZON.CancelIntent", "AMAZON.StopIntent"]:
-        return handle_session_end_request()
-    else:
-        raise ValueError("Invalid intent")
-
-
-def on_session_ended(session_ended_request, session):
-    """ Called when the user ends the session.
-
-    Is not called when the skill returns should_end_session=true
-    """
-    print("on_session_ended requestId=" + session_ended_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
-    # add cleanup logic here
-
-# --------------- Functions that control the skill's behavior ------------------
-
-
-def get_welcome_response():
-
-    session_attributes = {}
-    card_title = "Welcome"
-    speech_output = "We're about to embark on a magical journey. To get started, ask me to tell you about stanley. If you get lost, just ask for help."
-    # If the user either does not reply to the welcome message or says something
-    # that is not understood, they will be prompted again with the same text.
-    reprompt_text = speech_output
-    should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
-
-
-def get_help_response():
-    session_attributes = {}
-    card_title = "Help"
-    speech_output = "Stanley's journey is confusing sometimes. A couple of examples of phrases that I can accept are... Tell me about stanley or, who is stanley. Lets get started now by trying one of these."
-
-    reprompt_text = speech_output
-    should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(card_title,speech_output,reprompt_text,should_end_session))
-
-
+@ask.intent("TheBeginning")
 def stanleys_history():
-    session_attributes = {}
     card_title = "Stanley's Backstory"
-    speech_output = "So, you want to know about Stanley, right? Well, he's quite the character. He was born in the high sierra to a strong donkey daddy, and a beautiful mother. They loved him very much, but unforntunately he wasn't very smart. And to make matters worse, his back was pretty weak. As he got older, he was a burden on the family, so his parents took him into the woods one day and left him there to die. Isn't that sad? Do you want to know what happened next? If so, ask me what happened next?"
-    reprompt_text = speech_output
-    should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(card_title,speech_output,reprompt_text,should_end_session))
+    speech_output = render_template('TheBeginning')
+    return question(speech_output).simple_card(title=card_title, content=speech_output)
 
 
+@ask.intent("WhatHappenedNext")
 def what_happened_next():
-    session_attributes = {}
     card_title = "What Happened next?"
-    speech_output = "Well, everyone thought he died that night. Eaten by a wolf or a bear. Stanley's parents were quite sad. But Stanley didn't die. In fact he was adopted by a mountain lion family and spent years training to be a vicious mountain lion. After several years, he was strong and mean. And most importantly, ready for revenge. Want to know more? Ask me how does it end?"
-    reprompt_text = speech_output
-    should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(card_title,speech_output,reprompt_text,should_end_session))
+    speech_output = render_template('WhatHappenedNext')
+    return question(speech_output).simple_card(title=card_title, content=speech_output)
 
 
+@ask.intent("HowDoesItEnd")
 def how_does_it_end():
-    session_attributes = {}
     card_title = "The end of Stanley's Story"
-    speech_output = "Well one day, Stanley came down from the lions den to seek revenge. With blood on his mind, he entered the old stables. But when he saw his family, they were so shocked by his strength, wisdom and allure, that they accepted him back into the family. And they lived happily ever after. The end."
-    reprompt_text = speech_output
-    should_end_session = True
-    return build_response(session_attributes, build_speechlet_response(card_title,speech_output,reprompt_text,should_end_session))
+    speech_output = render_template('HowDoesItEnd')
+    return question(speech_output).simple_card(title=card_title, content=speech_output)
 
 
+## Stanley Learns to navigate
+section = "Stanley Learns to Navigate"
+@ask.intent("LearnToNavigate")
+def navigate_beginning():
+    speech_output = render_template('Lost_Beginning')
+    return question(speech_output).simple_card(title=section, content=speech_output)
 
-def handle_session_end_request():
-    card_title = "Session Ended"
-    speech_output = "Goodbye."
-    # Setting this to true ends the session and exits the skill.
-    should_end_session = True
-    return build_response({}, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
+@ask.intent("NextDay")
+def navigate_nextday():
+    speech_output = render_template('Lost_NextDay')
+    return question(speech_output).simple_card(title=section, content=speech_output)
 
+@ask.intent("BurnMarks")
+def navigate_fork():
+    speech_output = render_template('Lost_BurnMarks')
+    return question(speech_output).simple_card(title=section, content=speech_output)
 
-# --------------- Helpers that build all of the responses ----------------------
-
-
-def build_speechlet_response(title, output, reprompt_text, should_end_session):
-    return {
-        'outputSpeech': {
-            'type': 'PlainText',
-            'text': output
-        },
-        'card': {
-            'type': 'Simple',
-            'title': title,
-            'content': output
-        },
-        'reprompt': {
-            'outputSpeech': {
-                'type': 'PlainText',
-                'text': reprompt_text
-            }
-        },
-        'shouldEndSession': should_end_session
-    }
+@ask.intent("NoBurnMarks")
+def navigate_fork():
+    speech_output = render_template('Lost_NoBurnMarks')
+    return question(speech_output).simple_card(title=section, content=speech_output)
 
 
-def build_response(session_attributes, speechlet_response):
-    return {
-        'version': '1.0',
-        'sessionAttributes': session_attributes,
-        'response': speechlet_response
-    }
+@ask.intent("BridgeDecisionStream")
+def navigate_bridge_stream():
+    speech_output = render_template('Lost_Stream')
+    return question(speech_output).simple_card(title=section, content=speech_output)
+
+
+@ask.intent("BridgeDecisionNoStream")
+def navigate_bridge_nostream():
+    speech_output = render_template('Lost_NoStream')
+    return question(speech_output).simple_card(title=section, content=speech_output)
+
+
+## Necessary for working with a live Alexa Skill on the store, without these, it wont get published.
+
+@ask.intent('AMAZON.HelpIntent')
+def help():
+    card_title = "Help"
+    speech_output = render_template('Help')
+    return question(speech_output).simple_card(title=card_title, content=speech_output).reprompt(speech_output)
+
+@ask.intent('AMAZON.StopIntent')
+def stop():
+	return statement("Goodbye")
+
+@ask.intent('AMAZON.CancelIntent')
+def cancel():
+    return statement("Goodbye")
+
+
+@ask.session_ended
+def session_ended():
+    return "", 200
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
+#Potential stuff to log:
+    # log.info("Request ID: {}".format(request.requestId))
+    # log.info("Request Type: {}".format(request.type))
+    # log.info("Request Timestamp: {}".format(request.timestamp))
+    # log.info("Session New?: {}".format(session.new))
+    # log.info("User ID: {}".format(session.user.userId))
+    # log.info("Alexa Version: {}".format(version))
